Fix weighting

main.py
@@ -8,9 +8,9 @@ query = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
 
 # adjustable variables
 
-threshold = 1
-c_dist = 3
-c_sim = 5
+threshold = 1000
+c_dist = 5
+c_sim = 3
 
 # Input CSV
 
@@ -36,19 +36,34 @@ for row in laptops:
 print(laptops)
 print()
 
-# Find similarity using euclidean distance and cosine similarity
+# Find distance using euclidean distance and cosine distance
 
 nearest = np.empty((0, 2), dtype='object')
 
 for row in laptops:
+  passed = True
+  failed = True
+  for i in range(1, count_spec+1):
+    if row[i] < query[i-1]:
+      passed = False
+    if row[i] >= query[i-1]:
+      failed = False
+
   dist = np.linalg.norm(query - row[1:8])
-  sim = query.dot(row[1:8]) / (np.linalg.norm(query) * np.linalg.norm(row[1:8]))
+  sim = 1 - (query.dot(row[1:8]) / (np.linalg.norm(query) * np.linalg.norm(row[1:8])))
   val = (c_dist * dist + c_sim * sim) / (c_dist + c_sim)
+
+  if passed == True:
+    val += -100
+  if failed == True:
+    val += 100
+
   nearest = np.append(nearest, np.array([[row[0], val]], dtype='object'), axis=0)
 
-nearest = nearest[np.argsort(nearest[:,1])]
 
+nearest = nearest[np.argsort(nearest[:,1])]
 nearest = nearest[nearest[:,1] < threshold]
 
 print(f'Nearest laptops:\n{nearest}')
+print(len(nearest))
 